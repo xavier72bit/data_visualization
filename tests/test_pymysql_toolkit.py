3ce1f650236fdab9bc3921f2272448409c07fbf6
XaiverWu <Xavier72bit@163.com>
2023-06-20 21:46:25 +0800

    bugfix: 修复 PooledDB 的autocommit配置

--- tests/test_pymysql_toolkit.py
+++ tests/test_pymysql_toolkit.py
@@ -7,7 +7,8 @@ mysql_date_time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 class AccessLogDaoBaseTest(unittest.TestCase):
     """
-    对AccessLogDao的基本测试，access_log_id的UUID等先手动编了几个
+    对AccessLogDao的基本测试
+
     测试用例简述：
     1. 单条插入
     2. 多条插入（开启事务）
@@ -19,7 +20,7 @@ class AccessLogDaoBaseTest(unittest.TestCase):
     9. 多条删除（开启事务）
     """
 
-    def single_insert_and_single_select(self):
+    def test_single_insert_and_single_select(self):
         # 手动新建一个AccessLog对象
         access_log = AccessLog("9DA61402-A1CB-7041-A629-6191494C57DC",
                                mysql_date_time_now,
@@ -29,21 +30,25 @@ class AccessLogDaoBaseTest(unittest.TestCase):
                                "This is a test")
 
         # 将这个AccessLog对象信息插入数据库
-        with MysqlConnection as connection:
+        with MysqlConnection() as connection:
             # 新建一个AccessLogDao对象，将当前的连接的Cursor传入
             cursor = connection.get_cursor()
             access_log_dao = AccessLogDao(cursor)
             access_log_dao.insert_exc(access_log)
 
         # 验证过程，另外开启一个连接，查询
-        with MysqlConnection as connection:
+        with MysqlConnection() as connection:
             cursor = connection.get_cursor()
             access_log_dao = AccessLogDao(cursor)
             new_access_log = access_log_dao.select_one_exc_by_id(access_log)
 
         self.assertEqual(access_log.access_log_id, new_access_log.access_log_id)
-        self.assertEqual(access_log.access_date_time, new_access_log.access_date_time)
+        self.assertEqual(access_log.access_date_time, new_access_log.access_date_time.strftime("%Y-%m-%d %H:%M:%S"))
         self.assertEqual(access_log.access_token, new_access_log.access_token)
         self.assertEqual(access_log.access_state, new_access_log.access_state)
         self.assertEqual(access_log.delete_flag, new_access_log.delete_flag)
         self.assertEqual(access_log.access_log_message, new_access_log.access_log_message)
+
+
+if __name__ == '__main__':
+    unittest.main()

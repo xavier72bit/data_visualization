0557ece260a48997be9b7a9e208decdfec503da4
XaiverWu <Xavier72bit@163.com>
2023-06-20 12:01:34 +0800

    feat: [pymysql_toolkit]为PlotResultDao添加对应的数据库操作方法。

--- pymysql_toolkit.py
+++ pymysql_toolkit.py
@@ -238,7 +238,90 @@ class AccessLogDao:
 
 
 class PlotResultDao:
-    pass
+    def __init__(self, cursor):
+        self._execute_cursor = cursor
+
+    def insert_exc(self, plot_result: PlotResult):
+        insert_sql = 'INSERT INTO plot_result (plot_result_id, plot_task_id, plot_result_finish_date_time, plot_result_finish_state, plot_result_local_path, plot_result_upload_date_time, plot_result_upload_state, plot_result_url, delete_flag) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        params = (plot_result.plot_result_id, plot_result.plot_task_id, plot_result.plot_result_finish_date_time,
+                  plot_result.plot_result_finish_state, plot_result.plot_result_local_path,
+                  plot_result.plot_result_upload_date_time,
+                  plot_result.plot_result_upload_state, plot_result.plot_result_url, plot_result.delete_flag)
+
+        return self._execute_cursor.execute(insert_sql, params)
+
+    def delete_exc(self, plot_result: PlotResult):
+        delete_sql = 'DELETE FROM plot_result WHERE plot_result_id = %s'
+        params = (plot_result.plot_result_id,)
+
+        return self._execute_cursor.execute(delete_sql, params)
+
+    def update_exc(self, plot_result: PlotResult):
+        update_sql = 'UPDATE plot_result SET plot_task_id = %s, plot_result_finish_date_time = %s, plot_result_finish_state = %s, plot_result_local_path = %s, plot_result_upload_date_time = %s, plot_result_upload_state = %s, plot_result_url = %s, delete_flag = %s WHERE plot_result_id = %s'
+        params = (
+        plot_result.plot_task_id, plot_result.plot_result_finish_date_time, plot_result.plot_result_finish_state,
+        plot_result.plot_result_local_path, plot_result.plot_result_upload_date_time,
+        plot_result.plot_result_upload_state,
+        plot_result.plot_result_url, plot_result.delete_flag, plot_result.plot_result_id)
+
+        return self._execute_cursor.execute(update_sql, params)
+
+    def select_one_exc_by_id(self, plot_result: PlotResult):
+        select_one_by_id_sql = 'SELECT plot_result_id, plot_task_id, plot_result_finish_date_time, plot_result_finish_state, plot_result_local_path, plot_result_upload_date_time, plot_result_upload_state, plot_result_url, delete_flag FROM plot_result WHERE plot_result_id = %s LIMIT 0, 1'
+        params = (plot_result.plot_result_id,)
+
+        exc_result = self._execute_cursor.execute(select_one_by_id_sql, params)
+
+        # TODO: 换成日志
+        print(exc_result)
+
+        try:
+            select_result = self._execute_cursor.fetchone()
+            plot_result_result = PlotResult(select_result['plot_result_id'],
+                                            select_result['plot_task_id'],
+                                            select_result['plot_result_finish_date_time'],
+                                            select_result['plot_result_finish_state'],
+                                            select_result['plot_result_local_path'],
+                                            select_result['plot_result_upload_date_time'],
+                                            select_result['plot_result_upload_state'],
+                                            select_result['plot_result_url'],
+                                            select_result['delete_flag'])
+        except Exception as select_exc_err:
+            # TODO: 换成日志
+            print(select_exc_err)
+            return None
+        else:
+            return plot_result_result
+
+    def select_list_exc_by_plot_task_id(self, plot_result: PlotResult):
+        select_list_by_plot_task_id_sql = 'SELECT plot_result_id, plot_task_id, plot_result_finish_date_time, plot_result_finish_state, plot_result_local_path, plot_result_upload_date_time, plot_result_upload_state, plot_result_url, delete_flag FROM plot_result WHERE plot_task_id LIKE %s'
+        params = (plot_result.plot_task_id,)
+
+        exc_result = self._execute_cursor.execute(select_list_by_plot_task_id_sql, params)
+        # TODO: 换成日志
+        print(exc_result)
+        # TODO: 注意这里，如果有问题换成元组
+        plot_result_result_list = []
+
+        try:
+            for select_result in self._execute_cursor:
+                plot_result_result = PlotResult(select_result['plot_result_id'],
+                                                select_result['plot_task_id'],
+                                                select_result['plot_result_finish_date_time'],
+                                                select_result['plot_result_finish_state'],
+                                                select_result['plot_result_local_path'],
+                                                select_result['plot_result_upload_date_time'],
+                                                select_result['plot_result_upload_state'],
+                                                select_result['plot_result_url'],
+                                                select_result['delete_flag'])
+
+                plot_result_result_list.append(plot_result_result)
+        except Exception as select_exc_err:
+            # TODO: 换成日志
+            print(select_exc_err)
+            return None
+        else:
+            return plot_result_result_list
 
 
 class PlotTaskDao:

0aafab7f000d239e1ade9a0d4cfc5879f2a34d36
XaiverWu <Xavier72bit@163.com>
2023-06-20 19:53:07 +0800

    bugfix: 修复 project_common 模块的工作目录错误问题

--- project_common.py
+++ project_common.py
@@ -7,6 +7,6 @@ __all__ = ["CURRENT_WORKSPACE", "CURRENT_CONFIG_PATH", "CURRENT_LOG_PATH"]
 # -----------------------------------------------------
 
 # 工作目录
-CURRENT_WORKSPACE = os.path.abspath('.')
+CURRENT_WORKSPACE = os.path.split(os.path.abspath(__file__))[0]
 CURRENT_CONFIG_PATH = os.path.join(CURRENT_WORKSPACE, 'config')
 CURRENT_LOG_PATH = os.path.join(CURRENT_WORKSPACE, 'logs')
